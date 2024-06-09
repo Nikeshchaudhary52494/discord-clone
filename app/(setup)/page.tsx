608360819ec834d52035de40cbc8dc0bd06b1e6f
Nikesh Chaudhary <nikeshchaudhary52494@gmail.com>
2024-06-10 00:25:31 +0530

    prisma setup

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+import { initialProfile } from '@/lib/initial-profile';
+import { redirect } from 'next/navigation';
+import { db } from '@/lib/db';
+
+interface setupPageProps { }
+
+const SetupPage: FC<setupPageProps> = async ({ }) => {
+    const profile = await initialProfile();
+    const server = await db.server.findFirst({
+        where: {
+            members: {
+                some: {
+                    profileId: profile.id,
+                },
+            },
+        },
+    });
+
+    if (server) {
+        return redirect(`/servers/${server.id}`);
+    }
+
+    return <div>Create a server</div>
+};
+
+export default SetupPage;
