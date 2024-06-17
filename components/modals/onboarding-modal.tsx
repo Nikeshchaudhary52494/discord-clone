62e2e287f16d1ffa4d585de4a3b4fdfb172241c7
Nikesh Chaudhary <nikeshchaudhary52494@gmail.com>
2024-06-17 23:36:25 +0530

    servers api added

--- components/modals/onboarding-modal.tsx
+++ components/modals/onboarding-modal.tsx
@@ -26,7 +26,6 @@ import {
 
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
-// import FileUpload from '../file-upload';
 import { useRouter } from 'next/navigation';
 import FileUpload from './file-upload';
 
@@ -65,7 +64,9 @@ const OnboardingModal: FC<onboardingModalProps> = ({ }) => {
             router.refresh();
             window.location.reload();
             form.reset();
-        } catch (error) { }
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     if (!isMounted) return null;
