bc2f56e206de07fcd2579fc09ef1ec56460add26
Nikesh Chaudhary <nikeshchaudhary52494@gmail.com>
2024-06-29 02:19:50 +0530

    change in socket.io

--- components/providers/socket-provider.tsx
+++ components/providers/socket-provider.tsx
@@ -8,10 +8,10 @@ import {
     useState,
 } from 'react';
 
-import { io as ClientIO } from 'socket.io-client';
+import { io, Socket } from 'socket.io-client';
 
 type SocketContextType = {
-    socket: any | null;
+    socket: Socket | null;
     isConnected: boolean;
 };
 
@@ -25,12 +25,12 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = ({ children }: { children: ReactNode }) => {
-    const [socket, setSocket] = useState(null);
+    const [socket, setSocket] = useState<Socket | null>(null);
     const [isConnected, setIsConnected] = useState(false);
 
     useEffect(() => {
-        const socketInstance = new (ClientIO as any)(
-            process.env.NEXT_PUBLIC_SITE_URL!,
+        const socketInstance = io(
+            process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000',
             {
                 path: '/api/socket/io',
                 addTrailingSlash: false,
